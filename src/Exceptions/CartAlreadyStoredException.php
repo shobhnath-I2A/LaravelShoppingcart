3e225dc69fdb05865b2928ed3c432231d5e0e74e
Shobhnath-I2A <shobhnath.s@i2a.co>
2026-03-05 16:17:12 +0530

    First commit

---
+++ src/Exceptions/CartAlreadyStoredException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Shobhnath\Shoppingcart\Exceptions;
+
+use RuntimeException;
+
+class CartAlreadyStoredException extends RuntimeException {}
---
+++ src/Exceptions/InvalidRowIDException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Shobhnath\Shoppingcart\Exceptions;
+
+use RuntimeException;
+
+class InvalidRowIDException extends RuntimeException {}
